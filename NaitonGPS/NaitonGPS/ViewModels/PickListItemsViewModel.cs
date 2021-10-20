3d7e3556165f10538ec84afcae6da341b4341e6b
Artikov Umid <umiddeveloper@gmail.com>
2021-10-20 19:15:42 +0500

    Add project files.

---
+++ NaitonGPS/NaitonGPS/ViewModels/PickListItemsViewModel.cs
@@ -0,0 +1,108 @@
+﻿using NaitonGPS.Helpers;
+using NaitonGPS.Models;
+using NaitonGPS.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NaitonGPS.ViewModels
+{    
+    public class PickListItemsViewModel : BaseViewModel
+    {
+        private readonly int _pickListId;
+        private PickListItem _selectedItem;
+
+        public ObservableCollection<PickListItem> PicklistItems { get; set; }
+        public Command LoadItemsCommand { get; }
+        public Command StartEditCommand { get; }
+
+        public Command<PickListItem> ItemTapped { get; }
+        public Command<PickListItem> ChangeQuantityCommand { get; }
+        public Command<PickListItem> ChangeRackCommand { get; }
+
+        public bool IsEditable { get; set; }
+        public bool IsViewable { get; set; }
+
+        public PickListItemsViewModel(int pickListId)
+        {
+            _pickListId = pickListId;
+            Title = "Picklist";
+            PicklistItems = new ObservableCollection<PickListItem>();
+            LoadItemsCommand = new Command(async () => await LoadItems());
+            ItemTapped = new Command<PickListItem>(OnItemSelected);
+            ChangeQuantityCommand = new Command<PickListItem>(ChangeQuantity);
+            ChangeRackCommand = new Command<PickListItem>(ChangeRack);
+            StartEditCommand = new Command(StartEdit);
+
+            IsBusy = true;
+            LoadItems().GetAwaiter();
+            IsBusy = false;
+            IsViewable = true;            
+            SelectedItem = null;
+        }
+
+
+        async Task LoadItems()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var pickListItems = await Task.Run(()=> DataManager.GetPickListItems(_pickListId));
+                PicklistItems.Clear();
+                foreach (var item in pickListItems)
+                {
+                    PicklistItems.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public PickListItem SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                OnItemSelected(value);
+            }
+        }
+
+        async void OnItemSelected(PickListItem item)
+        {
+            if (item == null)
+                return;
+
+            //await Shell.Current.GoToAsync($"{nameof(PickListItems)}?{nameof(.ItemId)}={item}");            
+        }
+
+        async void ChangeQuantity(PickListItem item)
+        {
+            if (item == null) return;
+            await Shell.Current.Navigation.PushModalAsync(new PicklistQuantityBottomPopup(), true);
+        }
+
+        async void ChangeRack(PickListItem item)
+        {
+            if (item == null) return;
+            await Shell.Current.Navigation.PushModalAsync(new PicklistSearchItemBottomPopup(), true);
+        }
+
+        void StartEdit()
+        {            
+            IsEditable = true;
+            IsViewable = false;
+            OnPropertyChanged(nameof(IsViewable));
+            OnPropertyChanged(nameof(IsEditable));            
+        }
+    }
+}
---
+++ NaitonGPS/NaitonGPS/Views/PickList/PickListPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NaitonGPS.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NaitonGPS.Views.PickList
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PickListPage : ContentPage
+    {
+        PickListViewModel _viewModel;
+        public PickListPage()
+        {
+            InitializeComponent();            
+            BindingContext = _viewModel = new PickListViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
